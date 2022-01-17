fc2b6c745c205c5c4da169b73c266fbb71fe35b1
/Oꜰꜰʟɪɴᴇ ‌[ᴀꜰᴋ]「™ʟᴏɢ✓ᴏᴜᴛ❥︎✔︎ <94526115+Unknownvip@users.noreply.github.com>
2022-01-17 09:11:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2021 Janindu Malshan
+Copyright (c) 2021 Log Out Afk
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
